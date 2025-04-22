5cb478c0f4490d890c755594e22728d2b22500e4
NGEricPurnamaUtama <eric_purnama@yahoo.com>
2025-04-23 01:53:03 +0800

    Added feature.py that contains addNumbers function which adds 2 numbers and return the result

---
+++ feature.py
@@ -0,0 +1,9 @@
+# Done by: NG Eric Purnama Utama
+# 23 April 2025
+# Full Stack AI - NUS
+# Module 2 - Assignment
+# Task 2 - Create a New Branch and Implement a Feature
+
+def addNumbers(firstNo, secondNo):
+    total = firstNo + secondNo
+    return total
